fe36da172e268cb0494839dc276fa293d976e25c
Aleksandr Shkola <sprinterr23@gmail.com>
2022-09-04 20:07:55 +0300

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,8 +1,11 @@
-const BASE_URL = 'https://restcountries.com/v3.1/name/';
-const fields = 'fields = name,capital,population,flags,languages';
-
 export function fetchCountries(name) {
-  return fetch(`${BASE_URL}${name}?${fields}`)
-    .then(response => response.json())
-    .catch(error => console.log(error));
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+
+    return response.json();
+  });
 }
